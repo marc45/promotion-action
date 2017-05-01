262b84b15faae23ee83b1be04f6c3f4eac01a53a
shifeifei <1021376538@qq.com>
2017-05-01 22:40:50 +0800

    注解

---
+++ basic-point/src/main/java/com/jd/jr/annotation/PasswordUtil.java
@@ -0,0 +1,25 @@
+package com.jd.jr.annotation;
+
+import java.util.List;
+
+/**
+ * Created by shifeifei on 2017/5/1.
+ */
+public class PasswordUtil {
+
+    @UseCase(id=47,description = "password must contain at least one numeric")
+    public boolean validatePassword(String password){
+        return (password.matches("\\w*\\d\\w*"));
+    }
+
+    @UseCase(id=48)
+    public String encryptPassword(String password){
+        return new StringBuffer(password).reverse().toString();
+    }
+
+    @UseCase(id=49,description = "new password can't contain previous password")
+    public boolean checkForNewPassword(List<String> prePasswords,String password){
+        return prePasswords.contains(password);
+    }
+
+}
---
+++ basic-point/src/main/java/com/jd/jr/annotation/TestUseCase.java
@@ -0,0 +1,34 @@
+package com.jd.jr.annotation;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by shifeifei on 2017/5/1.
+ */
+public class TestUseCase {
+
+    public static void traceUseCases(List<Integer> useCases, Class<?> cla) {
+
+        for (Method m : cla.getDeclaredMethods()) {
+            UseCase uc = m.getAnnotation(UseCase.class);
+            if (null != uc) {
+                System.out.println("found use case :" + uc.id() + " " + uc.description());
+                useCases.remove(new Integer(uc.id()));
+            }
+        }
+
+        for (int i : useCases) {
+            System.out.println("warning : Missing use case - " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> useCases = new ArrayList<Integer>();
+        Collections.addAll(useCases, 47, 48, 49, 50);
+        traceUseCases(useCases, PasswordUtil.class);
+    }
+
+}
