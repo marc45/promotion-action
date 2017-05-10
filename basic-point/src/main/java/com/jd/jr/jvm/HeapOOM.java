d31b08ce0face8dab62a90eb8388fc9fe97b2dff
shifeifei <1021376538@qq.com>
2017-05-10 23:46:59 +0800

    active mq hello world

---
+++ basic-point/src/main/java/com/jd/jr/jvm/HeapOOM.java
@@ -0,0 +1,24 @@
+package com.jd.jr.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shifeifei on 2017/5/8.
+ */
+public class HeapOOM {
+
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+
+    }
+
+}
---
+++ jms-mq/src/main/java/com/jd/jr/mq/active/mq/Test.java
@@ -0,0 +1,7 @@
+package com.jd.jr.mq.active.mq;
+
+/**
+ * Created by shifeifei on 2017/5/10.
+ */
+public class Test {
+}
