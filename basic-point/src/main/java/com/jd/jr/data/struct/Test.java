843ba0c6f135832f91b6dc9d4b8aeedf52702429
shifeifei <shifeifei@jd.com>
2017-06-10 18:59:07 +0800

    线性表---->双向链表

--- basic-point/src/main/java/com/jd/jr/data/struct/Test.java
+++ basic-point/src/main/java/com/jd/jr/data/struct/Test.java
@@ -1,7 +1,20 @@
 package com.jd.jr.data.struct;
 
+import java.util.LinkedList;
+
 /**
  * Created by shifeifei on 2017/6/2.
  */
 public class Test {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedList = new LinkedList<String>();
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+
+        System.out.println(linkedList.toString());
+
+    }
 }
--- basic-point/src/main/java/com/jd/jr/data/struct/link/list/DoubleLinkedList.java
+++ basic-point/src/main/java/com/jd/jr/data/struct/link/list/DoubleLinkedList.java
@@ -17,8 +17,8 @@ public class DoubleLinkedList<T> {
      */
     private static class Node<T> {
         T element;
-        private Node<T> prior;
-        private Node<T> next;
+        private Node<T> prior;  //前驱结点
+        private Node<T> next;   //后继结点
 
         public Node(T element, Node<T> prior, Node<T> next) {
             this.element = element;
@@ -28,8 +28,8 @@ public class DoubleLinkedList<T> {
     }
 
     public DoubleLinkedList() {
-        //初始化一个空链表
-        header = header.next = header.prior;
+        //初始化一个空链表,前驱和后驱都指向自己
+        header.next = header.prior = header;
     }
 
     /**
@@ -38,7 +38,56 @@ public class DoubleLinkedList<T> {
      * @param e
      */
     public void add(T e) {
+        //创建一个新结点,设置它的前驱是原链表尾结点,后继是头节点
+        Node<T> node = new Node<T>(e, header.prior, header);
 
+        //设置该新结点的后驱结点(就是头结点header)的前驱
+        node.next.prior = node;
+
+        //设置该新结点的前驱结点(就是原链表尾结点)的后驱
+        node.prior.next = node;
+        size++;
+    }
+
+    /**
+     * 删除结点元素
+     */
+    public void delete(T e) {
+        Node<T> node = new Node<T>(e, null, null);
+        //遍历查找结点
+        for (Node<T> temp = header.next; temp != header; temp = temp.next) {
+            if (temp.element.equals(node.element)) {
+                temp.prior.next = temp.next;
+                temp.next.prior = temp.prior;
+
+                size--;
+            }
+        }
+    }
+
+    public void print() {
+        if (size < 1) {
+            throw new RuntimeException("空链表");
+        }
+        for (Node<T> temp = header.next; temp != header; temp = temp.next) {
+            System.out.println("元素结点: " + temp.element);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        DoubleLinkedList<String> linkedList = new DoubleLinkedList<String>();
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+        linkedList.add("d");
+
+        linkedList.print();
+
+        linkedList.delete("b");
+        System.out.println("------------------------------");
+
+        linkedList.print();
     }
 
 }
